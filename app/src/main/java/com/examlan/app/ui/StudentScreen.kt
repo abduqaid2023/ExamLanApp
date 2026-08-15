f78f4fc2c225ba6c5e54be0457dbf67695af5ad3
abduqaid2023 <142156024+abduqaid2023@users.noreply.github.com>
2026-08-15 15:43:44 +0300

    Add files via upload

--- app/src/main/java/com/examlan/app/ui/StudentScreen.kt
+++ app/src/main/java/com/examlan/app/ui/StudentScreen.kt
@@ -2,7 +2,6 @@ package com.examlan.app.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
